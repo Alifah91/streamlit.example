8b9220283b6c39b9ebd7a2965c8615af701849ac
Alifah91 <111938229+Alifah91@users.noreply.github.com>
2022-08-24 11:42:21 +0800

    Update iris-ml-app.py

--- iris-ml-app.py
+++ iris-ml-app.py
@@ -4,7 +4,7 @@ from sklearn import datasets
 from sklearn.ensemble import RandomForestClassifier
 
 st.write("""
-# Alifah91 -My First Application
+# Alifah91 - My First Application
 # Simple Iris Flower Prediction App
 
 This app predicts the **Iris flower** type!
